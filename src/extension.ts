1905b8a3e67919fa0ff1d84bc2a0fa18d96651c1
sketchbuch <dev.sketchbuch@gmail.com>
2024-07-14 10:57:41 +0200

    feat: got mouse position

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,11 @@
 import St from 'gi://St'
 
+import Shell from 'gi://Shell'
 import { Extension, ExtensionMetadata } from 'resource:///org/gnome/shell/extensions/extension.js'
 import * as Main from 'resource:///org/gnome/shell/ui/main.js'
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js'
+
+const Desktop = Shell.Global.get()
 
 export default class MouseCastExtension extends Extension {
   #indicator: null | PanelMenu.Button
@@ -13,8 +16,11 @@ export default class MouseCastExtension extends Extension {
   }
 
   enable() {
+    const [pointerX, pointerY, modifierType] = Desktop.get_pointer()
     // Create a panel button
     this.#indicator = new PanelMenu.Button(0.0, 'MouseCast', false)
+
+    console.log(`### ${pointerX}x${pointerY} - ${modifierType.toString()}`)
 
     // Add an icon
     const icon = new St.Icon({
