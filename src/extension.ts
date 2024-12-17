1566a62066554553152565761e7abbbe53bd496a
sketchbuch <dev.sketchbuch@gmail.com>
2024-12-17 16:35:47 +0100

    chore: add test menu

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@ import St from 'gi://St'
 import { Extension, ExtensionMetadata } from 'resource:///org/gnome/shell/extensions/extension.js'
 import * as Main from 'resource:///org/gnome/shell/ui/main.js'
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js'
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js'
 
 const Desktop = Shell.Global.get()
 
@@ -64,6 +65,14 @@ export default class MouseCastExtension extends Extension {
     this.#topbarButton.add_child(icon)
   }
 
+  createTopBarMenu() {
+    if (this.#topbarButton) {
+      const menu = new PopupMenu.PopupMenu(this.#topbarButton, 0.5, St.Side.TOP)
+      menu.addAction('Settings', () => console.log('activated'))
+      this.#topbarButton.setMenu(menu)
+    }
+  }
+
   createWidget() {
     this.#widget = new St.Widget({
       can_focus: false,
@@ -87,8 +96,10 @@ export default class MouseCastExtension extends Extension {
 
       if (needsOverlay) {
         const widgetOffset = this.#size / 2
-        const cursorOffsetX = 3
-        const cursorOffsetY = 6
+        /* const cursorOffsetX = 3
+        const cursorOffsetY = 6 */
+        const cursorOffsetX = 0
+        const cursorOffsetY = 0
 
         this.#widget.set_position(
           pointerX - (widgetOffset - cursorOffsetX),
@@ -130,6 +141,7 @@ export default class MouseCastExtension extends Extension {
     this.createOverlay()
     this.createWidget()
     this.createTopBarButton()
+    this.createTopBarMenu()
     this.addUi()
     this.setWidgetPosition()
     this.trackMouse()
